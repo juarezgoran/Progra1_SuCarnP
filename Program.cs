8510413d85d9cace09cde701ad3734c83480bc89
Goran Juarez <gjuarezv1@miumg.edu.gt>
2023-06-03 21:07:37 -0600

    Prueba Subida

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Definion de variables
+
+int valorA = 10;
+int valorB = 20;
+int resultadoSuma;
+int resultadoResta;
+int resultadoMultiplicacion;
+int resultadoDivision;
+
+Console.WriteLine("Bienvenido al programa que realiza varias operaciones");
+resultadoSuma = valorA + valorB;
+resultadoResta = valorA - valorB;
+resultadoMultiplicacion = valorA * valorB;
+resultadoDivision = valorA / valorB;
+
+
+Console.WriteLine("La suma del valor de a y b es: "+ resultadoSuma);
+Console.WriteLine("La suma del valor de a y b es: "+ resultadoResta);
+Console.WriteLine("La suma del valor de a y b es: "+ resultadoMultiplicacion);
+Console.WriteLine("La suma del valor de a y b es: "+ resultadoDivision);
+
+ 
